77b2d9d9cc912342f12ac9c4646ab7f09ee1c523
Aamir shaikh <aamirshaikh3232@outlook.com>
2023-06-13 18:48:08 +0530

    Add day_1_introduction_to_java_and_setup/level1/HelloWorld.java

---
+++ src/main/java/org/example/day_1_introduction_to_java_and_setup/level1/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.example.day_1_introduction_to_java_and_setup.level1;
+
+// Write a Java program to print "Hello, World!" to the console.
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
